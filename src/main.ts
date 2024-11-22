e8a055b8b43afbf0b850da13f3371a95a1f3c6f8
KODJO TAMEGNON ELISEE HENOC <helitako16@gmail.com>
2024-11-22 02:25:14 +0000

    Ajout de la configuration de dotenv et utilisation de la variable d'environnement PORT pour le démarrage du serveur

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000).then(() => console.log('Server started'));
+  await app
+    .listen(PORT)
+    .then(() =>
+      console.log('Server started on http://localhost:' + PORT + '/'),
+    );
 }
 bootstrap();
