54721d39d0d0acf24bdd0b20aa348a0b870d0cd5
KODJO TAMEGNON ELISEE HENOC <helitako16@gmail.com>
2024-11-24 22:24:14 +0000

    Ajout de la prise en charge CORS dans la configuration du serveur

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   await app
     .listen(PORT)
     .then(() =>
